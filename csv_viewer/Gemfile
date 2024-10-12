4744f5860de292fa1dc43ccecd3ea0d5253d57ad
zaynshawahna <136936745+zaynshawahna@users.noreply.github.com>
2024-10-12 04:41:44 +0300

    Delete csv_viewer directory

--- csv_viewer/Gemfile
+++
@@ -1,8 +0,0 @@
-# Gemfile
-source 'https://rubygems.org'
-
-gem 'tty-prompt'
-gem 'pry'
-gem 'csv'
-gem 'colorize'
-gem 'console_table'
--- csv_viewer/fetcher.rb
+++
@@ -1,9 +0,0 @@
-class Fetcher
-    def initialize(directory)
-      @directory = directory
-    end
-  
-    def fetch_csv_files
-      Dir.glob(File.join(@directory, '*.csv'))
-    end
-  end
